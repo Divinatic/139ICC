f81c73d9b58b419d53e34e131c52f7bed12805b0
Incentix <85671586+Divinatic@users.noreply.github.com>
2024-07-04 11:01:53 +1000

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
   return "Your Bot Is Ready"
 
 def run():
-  app.run(host="0.0.0.0", port=8000)
+  app.run(host="0.0.0.0", port=10000)
 
 def keep_alive():
   server = Thread(target=run)
